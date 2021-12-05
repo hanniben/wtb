bf27b2629452a08eb2a75cbe074cba1705862dad
hanniben <ash.kenworthy@gmail.com>
2021-12-05 16:15:29 -0500

    add gifs 1 and 2

--- javascript.js
+++ javascript.js
@@ -7,13 +7,13 @@ function initAll() {
 function gifOneAni() {
     timer = setTimeout("gifTwoAni()", 30000);
     document.getElementById("gifThree").src = "images/spicecake_still3.png";
-    document.getElementById("gifOne").src = "images/spicecake_gif1.gif";
+    document.getElementById("gifOne").src = "https://media2.giphy.com/media/MeVaDEhr0Smo5HeLtr/giphy.gif?cid=790b76112b187339c33365a53bba416ebfba8b23a299457b&rid=giphy.gif&ct=g";
 }
 
 function gifTwoAni() {
     timer = setTimeout("gifThreeAni()", 19000);
     document.getElementById("gifOne").src = "images/spicecake_still1.png";
-    document.getElementById("gifTwo").src = "images/spicecake_gif2.gif";
+    document.getElementById("gifTwo").src = "https://media0.giphy.com/media/57zZvwEdqTgvEF4trC/giphy.gif?cid=790b7611d1ceb021e5fda937af7fcbe58a768fbe1337aed4&rid=giphy.gif&ct=g";
 }
 
 function gifThreeAni() {
