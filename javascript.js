0edb776cec9e3e4274676091e898abade3d3dd43
hanniben <ash.kenworthy@gmail.com>
2021-12-05 16:02:27 -0500

    add gif3

--- javascript.js
+++ javascript.js
@@ -19,5 +19,5 @@ function gifTwoAni() {
 function gifThreeAni() {
     timer = setTimeout("gifOneAni()", 28000);
     document.getElementById("gifTwo").src = "images/spicecake_still2.png";
-    document.getElementById("gifThree").src = "images/spicecake_vid3.gif";
+    document.getElementById("gifThree").src = "https://media1.giphy.com/media/cVH9UVAfrntciKS5L5/giphy.gif?cid=790b7611a47bcf8cc4234bb84e295f16352f0d7ebdf23421&rid=giphy.gif&ct=g";
 }
